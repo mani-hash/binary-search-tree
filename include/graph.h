9e6b8b7ef861019f7411742a248b06e55c0b916d
mani-hash <manimehalan400@gmail.com>
2025-02-10 19:01:59 +0530

    feat: graph methods

---
+++ include/graph.h
@@ -0,0 +1,9 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include "types.h"
+
+Tree* initiateTree();
+void insertNode(Tree *tree, char* data);
+
+#endif // GRAPH_H
---
+++ src/graph.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "types.h"
+#include "graph.h"
+
+static Node* createNode(Node* parent, char* data);
+
+Tree* initiateTree()
+{
+    Tree* tree = malloc(sizeof(Tree));
+    tree->root = NULL;
+
+    return tree;
+}
+
+static Node* createNode(Node* parent, char* data)
+{
+    static char nodeName = 'a';
+
+    Node* node = malloc(sizeof(Node));
+
+    node->nodeName = nodeName;
+    node->data = data;
+    node->parent = parent;
+    node->left = NULL;
+    node->right = NULL;
+
+    nodeName += 1;
+
+    return node;
+}
+
+void insertNode(Tree *tree, char* data)
+{
+    
+    if (tree->root == NULL)
+    {
+        Node *node = createNode(NULL, data);
+        tree->root = node;
+
+        return;
+    }
+
+    Node *currentNode = tree->root;
+
+    while (currentNode != NULL)
+    {
+        int comparison =  strcmp(data, currentNode->data);
+
+        if (comparison < 0)
+        {
+            if (currentNode->left == NULL)
+            {
+                Node *node = createNode(currentNode, data);
+                currentNode->left = node;
+                break;
+            }
+            else
+            {
+                currentNode = currentNode->left;
+            }
+        }
+        else if (comparison > 0)
+        {
+            if (currentNode->right == NULL)
+            {
+                Node *node = createNode(currentNode, data);
+                currentNode->right = node;
+                break;
+            }
+            else
+            {
+                currentNode = currentNode->right;
+            }
+        }
+        else
+        {
+            perror("An error occured traversing the binary tree\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
