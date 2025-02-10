0f0fc06b3e25060f6f7f99acea17b96a0dacfa52
mani-hash <manimehalan400@gmail.com>
2025-02-10 21:36:37 +0530

    feat: updated logic code

--- src/logic.c
+++ src/logic.c
@@ -2,12 +2,23 @@
 #include "types.h"
 #include "decipherFile.h"
 #include "findHeight.h"
+#include "findSize.h"
+#include "findMin.h"
+#include "findMax.h"
 
 void run()
 {
     Tree *tree = getDataFromFile();
 
     printf("Height of tree is %d\n", findHeightOfTree(tree));
+    printf("Size of tree is %d\n", findSizeOfTree(tree));
+    
+    printf("\n");
+    displayMinimumNodeOfTree(findMinimumNodeOfTree(tree));
+    printf("\n");
+    displayMaximumNodeOfTree(findMaximumNodeOfTree(tree));
+    
+
 
     freeGraphData(tree);
 }
