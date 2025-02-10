8af12081975a1d8b4fc2efbae3dc6cc6f007a1aa
mani-hash <manimehalan400@gmail.com>
2025-02-10 20:42:13 +0530

    feat: added find tree height function

---
+++ include/actions/findHeight.h
@@ -0,0 +1,8 @@
+#ifndef FINDHEIGHT_H
+#define FINDHEIGHT_H
+
+#include "types.h"
+
+int findHeightOfTree(Tree *tree);
+
+#endif
---
+++ src/actions/findHeight.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "types.h"
+
+static int getMaxHeight(int height1, int height2);
+static int calculateMaxHeight(Node *node);
+
+static int getMaxHeight(int height1, int height2)
+{
+    if (height1 > height2)
+    {
+        return height1;
+    }
+
+    return height2;
+}
+
+static int calculateMaxHeight(Node *node)
+{
+    if (node == NULL)
+    {
+        return -1;
+    }
+    int leftHeight = calculateMaxHeight(node->left);
+    int rightHeight = calculateMaxHeight(node->right);
+
+    return getMaxHeight(leftHeight, rightHeight) + 1;
+}
+
+int findHeightOfTree(Tree *tree)
+{
+    return calculateMaxHeight(tree->root);
+}
